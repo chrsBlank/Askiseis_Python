0008f047f94a6113ac6ee3b77c7a184bbbd3e4dc
Christian(P17185) <christiangrece45@gmail.com>
2018-02-17 19:05:41 +0200

    post
    post askisi 7

--- askisi7Not_finished.py
+++ askisi7Not_finished.py
@@ -2,11 +2,6 @@ import datetime
 now = datetime.datetime.now()
 CurrMth = now.month
 CurrND = now.day
-CurrDay = 1 
-if (CurrDay > 1) :
- Diff = 7 - CurrDay
-else :
-    Diff = 0
 CurrYr = now.year
 TillYr = CurrYr + 10
 tp = 0
@@ -19,7 +14,6 @@ for i in range(CurrYr, TillYr + 1):
         TotalDays = TotalDays + 365
 
 NumbMon = 0
-TotalDays = TotalDays - Diff
 tp = 0
 for i in range(0, TotalDays):
     if (i % 7) == 0 :
